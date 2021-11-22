dc6cea66f5e896b859ae51c6b092e5d96ec17014
ititiu19223 <ititiu19223@student.hcmiu.edu.vn>
2021-11-22 15:08:31 +0700

    Ex 1, 2, 3, 4

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -12,21 +12,51 @@ Ex:
 #include <math.h>
 #include <string.h>
 
-void Ex2(int n, char *str[]){
+void Ex2(int n, char *str[])
+{
 	//Your codes here
-	
+	int arr[n];
+
+	for (int i = 0; i < n; i++)
+		arr[i] = i;
+
+	for (int i = 0; i < n; i++)
+		for (int j = i + 1; j < n; j++)
+		{
+			int a = 0;
+
+			while (str[arr[i]][a] != '\0')
+			{
+				if (str[arr[i]][a] > str[arr[j]][a])
+				{
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+					break;
+				}
+				else if (str[arr[i]][a] == str[arr[j]][a])
+					a++;
+				else
+					break;
+			}
+		}
+
+	for (int i = 0; i < n; i++)
+		printf("%s ", str[arr[i]]);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	//testing variable, applying it to your algorithm for auto-evaluating
 	argc--;
 	char *testcase[argc];
 	int i;
-	for(i=0; i<argc; i++){
-		testcase[i] = argv[i+1];
+	for (i = 0; i < argc; i++)
+	{
+		testcase[i] = argv[i + 1];
 	}
-	
+
 	Ex2(argc, testcase);
-	
+
 	return 0;
 }
--- src/Exercise3.c
+++ src/Exercise3.c
@@ -13,16 +13,73 @@ Ex:
 #include <math.h>
 #include <string.h>
 
-void Ex3(char *str){
+void Ex3(char *str)
+{
 	//Your codes here
-	
+	int len = strlen(str);
+	char longstr[1000], shortstr[1000];
+	int i = 0, count_long = 0, count_short = 0, longest = 0, shortest = 1000;
+	int idx_long, idx_short;
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] != ' ')
+		{
+			count_short++;
+			count_long++;
+		}
+		else
+		{
+			if (count_short < shortest)
+			{
+				shortest = count_short;
+				idx_short = i - shortest;
+			}
+			if (count_long > longest)
+			{
+				longest = count_long;
+				idx_long = i - longest;
+			}
+			count_short = 0;
+			count_long = 0;
+		}
+	}
+	if (count_short < shortest)
+	{
+		shortest = count_short;
+		idx_short = i - shortest;
+	}
+	if (count_long > longest)
+	{
+		longest = count_long;
+		idx_long = i - longest;
+	}
+
+	int j = 0;
+	for (int i = idx_long; i < (idx_long + longest); i++)
+	{
+		longstr[j] = str[i];
+		j++;
+	}
+
+	int k = 0;
+	for (int i = idx_short; i < (idx_short + shortest); i++)
+	{
+		shortstr[k] = str[i];
+		k++;
+	}
+	shortstr[k] = '\0';
+	longstr[j] = '\0';
+
+	printf("Shortest word: %s\n", shortstr);
+	printf("Longest word: %s", longstr);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	//testing variable, applying it to your algorithm for auto-evaluating
 	char *testcase = argv[1];
-	
+
 	Ex3(testcase);
-	
+
 	return 0;
 }
